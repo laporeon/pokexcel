72efa2203ef8c77b25d60ff5ac3a3f4e1e2d543c
laporeon <175569928+daspeon@users.noreply.github.com>
2025-01-02 15:29:03 -0300

    feat: create delete route

--- src/routes/pokemon.routes.js
+++ src/routes/pokemon.routes.js
@@ -8,5 +8,6 @@ const routes = Router();
 
 routes.get('/', pokemonController.get);
 routes.post('/', pokemonController.bulkCreate);
+routes.delete('/', pokemonController.bulkDelete);
 
 export { routes as pokemonRoutes };
