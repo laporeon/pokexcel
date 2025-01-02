d36c25457b3806cf6d0df8a4592724a463a72750
laporeon <175569928+daspeon@users.noreply.github.com>
2025-01-02 15:08:30 -0300

    feat: import logger and replace console with it

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 
 import routes from './routes/index.js';
+import { logger } from './utils/logger.js';
 
 const port = 3000 || process.env.PORT;
 
@@ -11,5 +12,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Server is running on port: http://localhost:${port}`);
+  logger.info(`Server is running at http://localhost:${port}`);
 });
